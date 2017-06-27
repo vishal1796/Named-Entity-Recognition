2910d2ff10aa38faea7041c171b448702ac8b374
vishal1796 <vkyadav.4242@gmail.com>
2017-06-28 02:23:28 +0530

    initial commit

---
+++ data_util.py
@@ -0,0 +1,195 @@
+import os
+import re
+import codecs
+import math
+import numpy
+from keras.preprocessing.sequence import pad_sequences
+
+
+def load_sentences(path):
+    """
+    Load sentences. A line must contain at least a word and its tag.
+    Sentences are separated by empty lines.
+    """
+    sentences = []
+    sentence = []
+    for line in codecs.open(path, 'r', 'utf8'):
+        line = re.sub('\d', '0', line.rstrip())
+        if not line:
+            if len(sentence) > 0:
+                if 'DOCSTART' not in sentence[0][0]:
+                    sentences.append(sentence)
+                sentence = []
+        else:
+            word = line.split()
+            assert len(word) >= 2
+            sentence.append(word)
+    if len(sentence) > 0:
+        if 'DOCSTART' not in sentence[0][0]:
+            sentences.append(sentence)
+    return sentences
+
+
+def update_tag_scheme(sentences):
+    """
+    Check and update sentences tagging scheme to IOBES.
+    Only IOB1 and IOB2 schemes are accepted.
+    """
+    for index, sentence in enumerate(sentences):
+        tags = [w[-1] for w in sentence]
+        
+        for i, tag in enumerate(tags):
+            if tag == 'O':
+                continue
+            split = tag.split('-')
+            if len(split) != 2 or split[0] not in ['I', 'B']:
+                raise Exception('Sentences should be given in IOB format! ' +
+                            'Please check sentence %d:\n%s' % (index, sentence))
+            if split[0] == 'B':
+                continue
+            elif i == 0 or tags[i - 1] == 'O':  # conversion IOB1 to IOB2
+                tags[i] = 'B' + tag[1:]
+            elif tags[i - 1][1:] == tag[1:]:
+                continue
+            else:  # conversion IOB1 to IOB2
+                tags[i] = 'B' + tag[1:]
+            
+        # new_tags = []
+        # for i, tag in enumerate(tags):
+        #     if tag == 'O':
+        #         new_tags.append(tag)
+        #     elif tag.split('-')[0] == 'B':
+        #         if i + 1 != len(tags) and \
+        #            tags[i + 1].split('-')[0] == 'I':
+        #             new_tags.append(tag)
+        #         else:
+        #             new_tags.append(tag.replace('B-', 'S-'))
+        #     elif tag.split('-')[0] == 'I':
+        #         if i + 1 < len(tags) and \
+        #                 tags[i + 1].split('-')[0] == 'I':
+        #             new_tags.append(tag)
+        #         else:
+        #             new_tags.append(tag.replace('I-', 'E-'))
+                                
+        for word, new_tag in zip(sentence, tags):
+            word[-1] = new_tag
+
+
+def build_vocab(documentlist):
+    """
+    Create a dictionary of items from a list of list of items.
+    Create a mapping (item to ID / ID to item) from a dictionary.
+    Items are ordered by decreasing frequency.
+    """
+    assert type(documentlist) is list
+    vocab = {}
+    for document in documentlist:
+        for item in document:
+            if item not in vocab:
+                vocab[item] = 1
+            else:
+                vocab[item] += 1
+    vocab['<UNK>'] = 10000000  
+    
+    sorted_vocab = sorted(vocab.items(), key=lambda x: (-x[1], x[0]))
+    id_to_word = {i: v[0] for i, v in enumerate(sorted_vocab)}
+    word_to_id = {v: k for k, v in id_to_word.items()}
+    return word_to_id
+
+
+def word_mapping(sentences, lower=False):
+    """
+    Create a dictionary and a mapping of words, sorted by frequency.
+    """
+    words = [[x[0].lower() if lower else x[0] for x in s] for s in sentences]
+    word_to_id = build_vocab(words)
+    print "Found %i unique words (%i in total)" % (len(word_to_id), sum(len(x) for x in words))
+    return word_to_id
+
+def char_mapping(sentences):
+    """
+    Create a dictionary and mapping of characters, sorted by frequency.
+    """
+    chars = ["".join([w[0] for w in s]) for s in sentences]
+    char_to_id = build_vocab(chars)
+    print "Found %i unique characters" % len(char_to_id)
+    return char_to_id
+
+def tag_mapping(sentences):
+    """
+    Create a dictionary and a mapping of tags, sorted by frequency.
+    """
+    tags = [[word[-1] for word in s] for s in sentences]
+    tag_to_id = build_vocab(tags)
+    print "Found %i unique named entity tags" % len(tag_to_id)
+    return tag_to_id
+
+def prepare_dataset(sentences, word_to_id, char_to_id, tag_to_id, lower=False):
+    
+    def f(x): return x.lower() if lower else x
+    data = []
+    for s in sentences:
+        str_words = [w[0] for w in s]
+        words = [word_to_id[f(w) if f(w) in word_to_id else '<UNK>'] for w in str_words]
+        chars = [[char_to_id[c] for c in w if c in char_to_id] for w in str_words]
+        tags = [tag_to_id[w[-1]] for w in s]
+        data.append({
+            'str_words': str_words,
+            'words': words,
+            'chars': chars,
+            'tags': tags,
+        })
+    return data
+
+
+def build_X_Y(dataSet, max_words=50, maxCharLength=20):
+    word_id = []
+    tag_id = []
+    char = []
+    sentencelist = []
+    char_sequence = []
+    
+    for row in dataSet:
+        sentence = row['str_words']
+        sentence = sentence[:max_words]
+        sentence += ['<UNK>'] * (max_words - len(sentence)) 
+        sentencelist.append(sentence)
+        
+    for row in dataSet:
+        wordlist = row['chars']
+        sentence = []
+        for word in wordlist[:max_words]:
+            word = word[:maxCharLength]
+            word += [0]*(maxCharLength -len(word))
+            sentence += word
+        sentence += [0] * maxCharLength * (max_words- len(wordlist))
+        char_sequence.append(sentence)
+        
+    for row in dataSet:
+        word_id.append(row['words'])
+        tag_id.append(row['tags'])
+        
+    word_id = pad_sequences(word_id, maxlen=max_words, padding='post', truncating='post', value=0)
+    tag_id = pad_sequences(tag_id, maxlen=max_words, padding='post', truncating='post', value=0)   
+    
+    return sentencelist, numpy.array(char_sequence), numpy.array(word_id), numpy.array(tag_id)
+
+
+def build_embeddingmatrix(word_to_id, word_embedding_dim=100):    
+    embeddings_index = {}
+    f = open(os.path.join('/glove/', 'glove.6B.100d.txt'))
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = numpy.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    print('Found %s word vectors.' % len(embeddings_index))
+
+    embedding_matrix = numpy.zeros((len(word_to_id) + 1, word_embedding_dim))
+    for word, i in word_to_id.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix   
---
+++ train.py
@@ -0,0 +1,109 @@
+import cPickle as pickle
+import numpy as np
+from data_util import *
+from crf import *
+from keras.layers import Input
+from keras.utils import to_categorical
+from keras.layers.embeddings import Embedding
+from keras.layers.convolutional import Conv1D
+from keras.layers.pooling import MaxPooling1D
+from keras.layers.recurrent import LSTM
+from keras.layers.wrappers import Bidirectional, TimeDistributed
+from keras.layers.core import Dense, Dropout
+from keras.layers.merge import Concatenate
+from keras.optimizers import SGD
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+
+def my_init(shape, dtype=None):
+    scale = np.sqrt(3.0 / 30)
+    return K.random_uniform(shape, minval=-scale, maxval=scale, dtype=dtype)
+
+
+train_sentences = load_sentences('/data/eng.train')
+valid_sentences = load_sentences('/data/eng.testa')
+
+update_tag_scheme(train_sentences)
+update_tag_scheme(valid_sentences)
+
+word_to_id = word_mapping(train_sentences + valid_sentences)
+embedding_matrix = build_embeddingmatrix(word_to_id)
+char_to_id, tag_to_id = char_mapping(train_sentences), tag_mapping(train_sentences)
+
+train_data = prepare_dataset(train_sentences, word_to_id, char_to_id, tag_to_id)
+valid_data = prepare_dataset(valid_sentences, word_to_id, char_to_id, tag_to_id)
+
+print "%i / %i  sentences in train / valid." % (len(train_data), len(valid_data))
+
+sentencelist_train, char_sequence_train, word_sequence_train, tag_sequence_train = build_X_Y(train_data)
+sentencelist_valid, char_sequence_valid, word_sequence_valid, tag_sequence_valid = build_X_Y(valid_data)
+
+tags_train = []
+tags_valid = []
+
+for i in range(len(tag_sequence_train)):
+    tags_train.append(to_categorical(tag_sequence_train[i], 18))
+for i in range(len(tag_sequence_valid)):
+    tags_valid.append(to_categorical(tag_sequence_valid[i], 18))
+    
+tags_train = np.array(tags_train)
+tags_valid = np.array(tags_valid)
+
+print('char_sequence_train shape:', char_sequence_train.shape)
+print('word_sequence_train shape:', word_sequence_train.shape)
+print('tag_sequence_train shape:', tag_sequence_train.shape)
+print('char_sequence_valid shape:', char_sequence_valid.shape)
+print('word_sequence_valid shape:', word_sequence_valid.shape)
+print('tag_sequence_valid shape:', tag_sequence_valid.shape)
+
+pickle.dump(word_to_id, open("/output/word_to_id.pkl", 'wb'))
+pickle.dump(char_to_id, open("/output/char_to_id.pkl", 'wb'))
+pickle.dump(tag_to_id, open("/output/tag_to_id.pkl", 'wb'))
+
+char_embedding_dim = 30
+word_embedding_dim = 100
+lstm_dim = 200
+max_words = 50
+maxCharSize = 20 
+word_vocab_size = len(word_to_id)
+char_vocab_size = len(char_to_id)
+tag_label_size = len(tag_to_id)
+
+
+print('Train...')
+char_input = Input(shape=(maxCharSize * max_words,), dtype='int32', name='char_input')
+char_emb = Embedding(char_vocab_size, char_embedding_dim, embeddings_initializer=my_init, input_length = max_words*maxCharSize, name='char_emb')(char_input)
+char_emb = Dropout(0.5)(char_emb)
+char_cnn = Conv1D(filters=30, kernel_size=3, activation='relu', padding='same')(char_emb)
+char_max_pooling = MaxPooling1D(pool_size=maxCharSize)(char_cnn)
+word_input = Input(shape=(max_words,), dtype='int32', name='word_input')
+word_emb = Embedding(word_vocab_size+1, word_embedding_dim, weights=[embedding_matrix], name='word_emb')(word_input)
+final_emb = Concatenate(axis=2, name='final_emb')([word_emb, char_max_pooling])
+emb_droput = Dropout(0.5)(final_emb)
+bilstm_word = Bidirectional(LSTM(200, kernel_initializer='glorot_uniform', return_sequences=True, unit_forget_bias=True))(emb_droput)
+bilstm_word_d = Dropout(0.5)(bilstm_word)
+dense = TimeDistributed(Dense(tag_label_size))(bilstm_word_d)
+crf = ChainCRF()
+crf_output = crf(dense)
+
+model = Model(inputs=[char_input, word_input], outputs=crf_output)
+
+sgd = SGD(lr=0.015, decay=0.05, momentum=0.9, nesterov=False, clipvalue=5)
+model.compile(loss=crf.loss, optimizer=sgd, metrics=['accuracy'])
+
+filepath = "/output/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list=[checkpoint]
+
+model.fit([char_sequence_train, word_sequence_train], tags_train,
+           validation_data=([char_sequence_valid, word_sequence_valid], tags_valid),
+            batch_size=10, epochs=50, callbacks=callbacks_list)
+
+model_json = model.to_json()
+with open("/output/model_json.json", "w") as json_file:
+    json_file.write(model_json)
+
+model.save_weights("/output/full_model_weight.h5")
+
+model.save('/output/bilstm-cnn-crf.h5')
